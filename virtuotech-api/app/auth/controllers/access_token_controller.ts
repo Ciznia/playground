21464f9e9d9a59549cf7c9bc1388c78f09a5a2b9
ciznia <hosquetgabriel@gmail.com>
2026-04-14 11:37:28 +0200

    document all existing endpoint
    typing for some is not done since i failed to make the return type understanble by `@foadonis/openapi`

--- virtuotech-api/app/auth/controllers/access_token_controller.ts
+++ virtuotech-api/app/auth/controllers/access_token_controller.ts
@@ -2,8 +2,23 @@ import User from '#app/auth/models/user'
 import { loginValidator } from '#app/auth/validators/user'
 import type { HttpContext } from '@adonisjs/core/http'
 import UserTransformer from '#app/auth/transformers/user_transformer'
+import { ApiOperation, ApiBody, ApiResponse } from '@foadonis/openapi/decorators'
 
 export default class AccessTokenController {
+  @ApiOperation({
+    summary: 'Create a new access token',
+    description: 'Generates a new access token for an existing user.',
+  })
+  @ApiBody({
+    description: 'User login credentials',
+    required: true,
+    type: () => loginValidator.schema,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Access token generated successfully',
+    // Same issue with typing as in new_account_controller.ts
+  })
   async store({ request, serialize }: HttpContext) {
     const { email, password } = await request.validateUsing(loginValidator)
 
@@ -15,6 +30,24 @@ export default class AccessTokenController {
       token: token.value!.release(),
     })
   }
+
+  @ApiOperation({
+    summary: 'Revoke the current access token',
+    description: 'Logs out the user by revoking their current access token.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User logged out successfully',
+    schema: {
+      type: 'object',
+      properties: {
+        message: {
+          type: 'string',
+          example: 'Logged out successfully',
+        },
+      },
+    },
+  })
 
   async destroy({ auth }: HttpContext) {
     const user = auth.getUserOrFail()
--- virtuotech-api/app/auth/controllers/new_account_controller.ts
+++ virtuotech-api/app/auth/controllers/new_account_controller.ts
@@ -2,8 +2,29 @@ import User from '#app/auth/models/user'
 import { signupValidator } from '#app/auth/validators/user'
 import type { HttpContext } from '@adonisjs/core/http'
 import UserTransformer from '#app/auth/transformers/user_transformer'
+import { ApiOperation, ApiBody, ApiResponse } from '@foadonis/openapi/decorators'
+
 
 export default class NewAccountController {
+  @ApiOperation({
+    summary: 'Create a new user account',
+    description: 'Registers a new user and returns an access token.',
+  })
+  @ApiBody({
+    description: 'User registration data',
+    required: true,
+    type: () => signupValidator.schema,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User account created successfully',
+    // It's possible to define custom types for the response but i didn't succeed to make
+    // it work with this response structure that use a transformer internally
+    // or we can just specify a static response schema exemple but it's verbose as hell
+  })
+  // It's also possible to use the ApiResponse decorator multiple times when we want to define
+  // multiple response schemas for different status code but here the only possible response
+  // other than 201 is the default validation error respone 422 or 500 which are global but not yet documented
   async store({ request, serialize }: HttpContext) {
     const { fullName, email, password } = await request.validateUsing(signupValidator)
 
