2e59c2dd68f8ef7b96e989d38282c5efe51cc2ab
ciznia <hosquetgabriel@gmail.com>
2026-04-14 17:04:53 +0200

    migrate db to remove authentification for now

---
+++ virtuotech-api/database/migrations/1776178314129_alter_users_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('email')
+      table.dropColumn('password')
+      table.dropColumn('full_name')
+      table.string('family_name').notNullable()
+      table.string('given_name').notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('full_name').nullable()
+      table.string('email', 254).notNullable().unique()
+      table.string('password').notNullable()
+      table.dropColumn('family_name')
+      table.dropColumn('given_name')
+    })
+  }
+}
