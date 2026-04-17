bc06aa9023c2183d086bfb91c3bd6eb76179e714
Antoine Quillet <antoine.quillet@epitech.eu>
2026-04-17 14:26:15 +0200

    fix([VS-432]): setting up environment variables for unit tests

---
+++ virtuotech-api/tests/functional/routes/user.spec.ts
@@ -0,0 +1,12 @@
+import { test } from '@japa/runner'
+
+test.group('check "Hello" root route', () => {
+  test('GET root returns 200', async ({ client }) => {
+    const response = await client.get('/')
+
+    response.assertStatus(200)
+    response.assertBody({
+      hello: 'world',
+    })
+  })
+})
